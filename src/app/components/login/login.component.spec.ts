e1945e1e82d858723300ece7c82c8c2061e9f4d2
Camilo Muñoz <jcamilomunozz@gmail.com>
2020-04-24 11:58:19 -0500

    FieldValidity

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -24,4 +24,25 @@ describe('LoginComponent', () => {
     expect(component.form.valid).toBeFalsy();
   });
 
+  it('should have email field invalid on init', () => {
+    component.ngOnInit();
+    const emailControl = component.form.controls['email'];
+    expect(emailControl.valid).toBeFalsy();
+  });
+
+  it('should validate email required fail', () => {
+    component.ngOnInit();
+    const emailControl = component.form.controls['email'];
+    const errors = emailControl.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+
+  it('should validate email required success', () => {
+    component.ngOnInit();
+    const emailControl = component.form.controls['email'];
+    emailControl.setValue('pablo@mail.com');
+    const errors = emailControl.errors || {};
+    expect(errors['required']).toBeFalsy();
+  });
+
 });
